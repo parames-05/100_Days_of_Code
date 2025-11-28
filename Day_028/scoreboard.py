13e8732b1e46234b3e88ea73c8f51b23165e8e6f
Parameswaran M V <waranparames80@gmail.com>
2025-11-28 22:31:44 +0530

    The following directories from day 26 to 30 are added

---
+++ Day_028/scoreboard.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.color("white")
+        self.goto(0,270)
+        self.write(f"Score: {self.score}", align="centre", font=("Arial",24,"normal"))
+        self.hideturtle()
+
+    def increase_score(self):
+        self.score +=1
+        self.clear()
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
